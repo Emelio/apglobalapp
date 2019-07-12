b152adef3f6c6df308c61520506dd35a26db77e6
Emelio <emeliocampbell@hotmail.co.uk>
2019-07-12 12:42:29 -0400

    update

--- apglobal/lib/screens/billing/home.dart
+++ apglobal/lib/screens/billing/home.dart
@@ -8,52 +8,51 @@ class BillingHome extends StatefulWidget {
 class BillingHomeState extends State<BillingHome> {
 
   buttons(String name) {
+
     return Container(
-      margin: EdgeInsets.symmetric(vertical: 20),
       width: 120,
-      height: 80,
-      padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
-      decoration: BoxDecoration(
-        // Box decoration takes a gradient
-        gradient: LinearGradient(
-          // Where the linear gradient begins and ends
-          begin: Alignment.topRight,
-          end: Alignment.bottomLeft,
-          // Add one stop for each color. Stops should increase from 0 to 1
-          stops: [0.1, 0.5, 0.7, 0.9],
-          colors: [
-            // Colors are easy thanks to Flutter's Colors class.
-            Colors.blue[800],
-            Colors.blue[600],
-            Colors.blue[600],
-            Colors.blue[800],
-          ],
-        ),
-      ),
-      child: Text('$name', style: TextStyle(color: Colors.white,), textAlign: TextAlign.center,),
+      child: RaisedButton(onPressed: (){}, child: Text('$name', style: TextStyle(color: Colors.white,), textAlign: TextAlign.center,),),
     );
+//    return Container(
+//      margin: EdgeInsets.symmetric(vertical: 20),
+//      width: 120,
+//      height: 80,
+//      padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+//      decoration: BoxDecoration(
+//        // Box decoration takes a gradient
+//        gradient: LinearGradient(
+//          // Where the linear gradient begins and ends
+//          begin: Alignment.topRight,
+//          end: Alignment.bottomLeft,
+//          // Add one stop for each color. Stops should increase from 0 to 1
+//          stops: [0.1, 0.5, 0.7, 0.9],
+//          colors: [
+//            // Colors are easy thanks to Flutter's Colors class.
+//            Colors.blue[800],
+//            Colors.blue[600],
+//            Colors.blue[600],
+//            Colors.blue[800],
+//          ],
+//        ),
+//      ),
+//      child: Text('$name', style: TextStyle(color: Colors.white,), textAlign: TextAlign.center,),
+//    );
   }
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-        appBar: AppBar(title: Text('Subscription Status', style: TextStyle(fontSize: 14),), centerTitle: true, elevation: 0,),
+        appBar: AppBar(title: Text('BILLING', style: TextStyle(fontSize: 20),), centerTitle: true, backgroundColor: Color(0xFF15DED0),
+        bottom: PreferredSize(child: Padding(padding: EdgeInsets.symmetric(vertical: 10), child:
+        Column(children: <Widget>[
+        Text('Subscription Status', style: TextStyle(color: Colors.white, fontSize: 14, fontWeight: FontWeight.bold),),
+          Text('Expires March 12, 2018', style: TextStyle(color: Colors.white, fontSize: 12))
+          ],),),
+            preferredSize: Size.fromHeight(30.0)),),
         body: Column(
           children: <Widget>[
-            Container(
-              color: Color(0xFF0081b0),
-              padding: EdgeInsets.symmetric(vertical: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Column(children: <Widget>[
-                    Text('ACTIVE', style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),),
-                    Text('March 20, 2018 to March 12, 2018', style: TextStyle(color: Colors.white, fontSize: 12))
-                  ],)
-                ],
-              ),
-            ),
+
             Column(
               children: <Widget>[
                 Row(
@@ -73,8 +72,8 @@ class BillingHomeState extends State<BillingHome> {
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: <Widget>[
-                    buttons("Order Device (Coming soon)"),
-                    buttons("Car Care (Coming soon)"),
+                    buttons("Order Device "),
+                    buttons("Car Care"),
                   ],
                 )
               ],
